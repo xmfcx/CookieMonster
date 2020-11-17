9f435b2ea140b8a98d04fa04640c039b1eaef888
M. Fatih Cırıt <xmfcx@users.noreply.github.com>
2020-11-17 12:30:19 +0300

    Update CookieMonster.js

--- CookieMonster.js
+++ CookieMonster.js
@@ -2964,7 +2964,7 @@ CM.DelayInit = function() {
 	if (Game.prefs.popups) Game.Popup('Cookie Monster version ' + CM.VersionMajor + '.' + CM.VersionMinor + ' loaded!');
 	else Game.Notify('Cookie Monster version ' + CM.VersionMajor + '.' + CM.VersionMinor + ' loaded!', '', '', 1, 1);
 
-	Game.Win('Third-party');
+	//Game.Win('Third-party');
 }
 
 CM.HasReplaceNativeGrimoireLaunch = false;
